59691f39b274a737d068cba9b299737abaf9d734
mauro-angel99 <angelottimauro@gmail.com>
2022-08-25 18:03:58 +0200

    props footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <TheHeader :sideMenu="sideMenu" />
     <BaseMain />
+    <TheFooter :links="links" :contacts="contacts" />
   </div>
 </template>
 
@@ -9,6 +10,9 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import TheHeader from './components/TheHeader.vue'
 import BaseMain from './components/BaseMain.vue'
+import TheFooter from './components/TheFooter.vue'
+
+
 
 
 export default {
@@ -16,10 +20,13 @@ export default {
   components: {
     TheHeader,
     BaseMain,
+    TheFooter,
   },
   data() {
     return {
-      sideMenu: ['home', 'About Me', 'Testimonials', 'My Blog', 'Meetups', 'Shop', 'Contact Me']
+      sideMenu: ['home', 'About Me', 'Testimonials', 'My Blog', 'Meetups', 'Shop', 'Contact Me'],
+      links: [`Latest Books`, 'Upcoming Events', 'Recent Articles', 'Business Enquiries', 'Visit My Foundation'],
+      contacts: ['63 E. Studebaker Street Bolingbrook, IL 60440', 'Phone: (800) 000-0000', 'mobile: (800) 000-0000', 'Email: info@your-company.com']
     }
   }
 }
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,52 @@
+<template>
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-3">
+                    <img src="../assets/images/author-logo-round-small.png" alt="">
+                </div>
+                <div class="col-3">
+                    <h4>ABOUT ME</h4>
+                    <P>
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Nulla quis lorem ut libero malesuada
+                        feugiat. Vivamus suscipit tortor eget felis porttitor volutpat.
+                    </P>
+                </div>
+                <div class="col-3">
+                    <h4>USEFUL LINKS</h4>
+                    <ul>
+                        <li li v-for="link in links" :key="link">{{ link }}</li>
+                    </ul>
+                </div>
+                <div class="col-3">
+                    <h4>CONTACT INFO</h4>
+                    <ul>
+                        <li li v-for="contact in contacts" :key="contact">{{ contact }}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+
+export default {
+    name: 'TheFooter',
+    props: {
+        links: Array,
+        contacts: Array,
+
+    },
+}
+</script>
+
+<style lang="scss">
+footer {
+    height: 400px;
+    background-image: url(../assets/images/footer-3.jpg);
+    background-size: cover;
+    background-position-y: top;
+    color: white;
+}
+</style>
